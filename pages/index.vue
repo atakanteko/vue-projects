7d4f7be1a5372ccdbd57fac33ca48f23f21c686f
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-02 22:11:28 +0300

    add home page components

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-
+  <main>
+    <SearchForm />
+    <CocktailList />
+  </main>
 </template>
 
 <script>
+import SearchForm from "../components/SearchForm";
+import CocktailList from "../components/CocktailList";
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components:{
+    SearchForm,
+    CocktailList
+  }
 }
 </script>
