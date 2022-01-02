0b54c881c34c8c106136ae10ef7a382a80bace84
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-02 19:37:03 +0300

    scaffold project structure

---
+++ components/CocktailList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>cocktail list component</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ components/SearchForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>search form component</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
