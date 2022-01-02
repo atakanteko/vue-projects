e11270296c03f0225311285e08ea844ceeb6dbbd
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-02 22:07:50 +0300

    add about page and error page

---
+++ layouts/error.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="error-page section">
+    <div class="error-container">
+      <h1>oops! it's a dead end</h1>
+      <nuxt-link to="/" class="btn btn-primary">back home</nuxt-link>
+    </div>
+  </section>
+</template>
