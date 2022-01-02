83676f1c3546a682b75186cab27bead1e21c82d3
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-02 22:03:24 +0300

    add navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    <h2>navbar component</h2>
-  </div>
+  <nav class="navbar">
+    <div class="nav-center">
+      <nuxt-link to="/"><img src="~/assets/img/logo.svg" /></nuxt-link>
+      <ul class="nav-links">
+        <li>
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/about">About</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
+
 export default {
 
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,16 @@
 <template>
-  <Nuxt />
+  <div>
+    <Navbar />
+    <Nuxt />
+  </div>
 </template>
+
+<script>
+import Navbar from "../components/Navbar";
+export default {
+
+  components:{
+    Navbar
+  }
+}
+</script>
