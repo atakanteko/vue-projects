e1a37ab6123102d3b36be174b42066e875cd2c71
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-03 18:38:17 +0300

    fix nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,6 @@
 export default {
+  ssr:false,
+  target:'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: '03-cocktails',
@@ -41,5 +43,8 @@ export default {
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  generate: {
+    fallback: true
   }
 }
