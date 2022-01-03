50175b3bda944a8d26bdfd22c9bebdb395d6727c
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-03 18:31:05 +0300

    show cocktail details

---
+++ components/SingleCocktail.vue
@@ -0,0 +1,56 @@
+<template>
+  <main>
+
+    <section class='section cocktail-section'>
+      <nuxt-link to="/" class="btn btn-primary">Home</nuxt-link>
+      <h2 class='section-title'>{{this.newCocktail.cocktailName}}</h2>
+      <div class='drink'>
+        <img :src="this.newCocktail.image" :alt="this.newCocktail.cocktailName">
+        <div class='drink-info'>
+          <p>
+            <span class='drink-data'>name :</span> {{this.newCocktail.cocktailName}}
+          </p>
+          <p>
+            <span class='drink-data'>category :</span> {{this.newCocktail.category}}
+          </p>
+          <p>
+            <span class='drink-data'>info :</span> {{this.newCocktail.info}}
+          </p>
+          <p>
+            <span class='drink-data'>glass :</span> {{this.newCocktail.glass}}
+          </p>
+          <p>
+            <span class='drink-data'>instructons :</span> {{this.newCocktail.instructions}}
+          </p>
+          <p>
+            <span class='drink-data'>ingredients :</span>
+              <span v-for="(item, index) in this.newCocktail.ingredients" :key="index"> {{ item }}</span>
+          </p>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import SearchForm from "../components/SearchForm";
+import CocktailList from "../components/CocktailList";
+export default {
+
+  components:{
+    SearchForm,
+    CocktailList
+  },
+  data() {
+    return {
+      cocktail: null
+    }
+  },
+  props:{
+    newCocktail:{
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- pages/cocktail/_id/index.vue
+++ pages/cocktail/_id/index.vue
@@ -1,5 +1,54 @@
 <template>
   <div>
-    single cocktail page
+    <SingleCocktail :new-cocktail="newCocktail"/>
   </div>
 </template>
+
+<script>
+import SingleCocktail from '../../../components/SingleCocktail'
+import axios from 'axios';
+export default {
+  components:{
+    SingleCocktail
+  },
+  async asyncData({ params }) {
+    console.log("id", params.id)
+    const drink = await axios.get(
+      `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${params.id}`
+    )
+
+    if (drink.data.drinks[0]) {
+      const {
+        strDrink: cocktailName,
+        strDrinkThumb: image,
+        strAlcoholic: info,
+        strCategory: category,
+        strGlass: glass,
+        strInstructions: instructions,
+        strIngredient1,
+        strIngredient2,
+        strIngredient3,
+        strIngredient4,
+        strIngredient5,
+      } = drink.data.drinks[0]
+      const ingredients = [
+        strIngredient1,
+        strIngredient2,
+        strIngredient3,
+        strIngredient4,
+        strIngredient5,
+      ]
+      const newCocktail = {
+        cocktailName,
+        image,
+        info,
+        category,
+        glass,
+        instructions,
+        ingredients,
+      }
+      return { newCocktail };
+    }
+  }
+}
+</script>
